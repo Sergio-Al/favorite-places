80d3e9d87680ebf21e9f33025f39604cef2b5d54
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-08-26 16:38:00 -0400

    Outputting the location data

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -35,7 +35,18 @@ class PlacesList extends StatelessWidget {
               radius: 26,
               backgroundImage: FileImage(places[index].image),
             ),
-            title: Text(places[index].name),
+            title: Text(
+              places[index].name,
+              style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              places[index].location.address,
+              style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
           ),
         );
       },
