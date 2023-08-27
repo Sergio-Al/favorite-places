1c55c40002ed0ae7114ed69971ec543c8884d586
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2023-08-26 21:27:14 -0400

    Storing Place Data in a SQL Database

--- lib/providers/favorite_places_provider.dart
+++ lib/providers/favorite_places_provider.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 import 'package:favorite_places_sergio/models/place.dart';
 
@@ -14,7 +16,28 @@ class FavoritePlacesNotifier extends StateNotifier<List<Place>> {
     final filename = path.basename(image.path);
     final copiedImage = await image.copy('${appDir.path}/$filename');
 
-    final newPlace = Place(name: title, image: copiedImage, location: location);
+    final newPlace = Place(
+      name: title,
+      image: copiedImage,
+      location: location,
+    );
+
+    final dbPath = await sql.getDatabasesPath();
+
+    final db = await sql.openDatabase(path.join(dbPath, 'places.db'),
+        onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address: TEXT)');
+    }, version: 1);
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.name,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.latitude,
+      'lng': newPlace.location.longitude,
+      'address': newPlace.location.address,
+    });
+
     state = [...state, newPlace];
   }
 }
